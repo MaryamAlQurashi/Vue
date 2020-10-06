cb11276ade07139948c79e4981ed4da1878b1950
MaryamAlQurashi <70502395+MaryamAlQurashi@users.noreply.github.com>
2020-10-06 11:15:26 +0400

    Update app.module.ts

--- src/app.module.ts
+++ src/app.module.ts
@@ -2,12 +2,10 @@ import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { MongooseModule } from '@nestjs/mongoose';
-import { CustomerModule } from './customer/customer.module';
 import { PacketModule } from './packet/packet.module';
 @Module({
   imports: [
     MongooseModule.forRoot('mongodb://localhost/database', { useNewUrlParser: true }),
-    CustomerModule,
     PacketModule
   ],
   controllers: [AppController],
